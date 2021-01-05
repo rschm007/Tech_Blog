0f122d9289faf98bad498ac867fc4d0a9ea70b8d
Robert Schmahl <rschm007@gmail.com>
2021-01-04 18:58:02 -0800

    user can now login. Logging in renders profile page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,7 +48,7 @@ router.get("/profile", withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ["password"] },
-      include: [{ model: Project }],
+      include: [{ model: Blog }],
     });
 
     const user = userData.get({ plain: true });
@@ -58,6 +58,7 @@ router.get("/profile", withAuth, async (req, res) => {
       logged_in: true,
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
@@ -66,6 +67,7 @@ router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect("/");
+    console.log("User is already logged in");
     return;
   }
 

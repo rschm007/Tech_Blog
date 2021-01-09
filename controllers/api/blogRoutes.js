874ec7ccc5206714ca46c4b68764959047205b34
Robert Schmahl <rschm007@gmail.com>
2021-01-09 14:11:16 -0800

    heroku build

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -5,12 +5,12 @@ const withAuth = require("../../utils/auth");
 // POST a blog
 router.post("/", withAuth, async (req, res) => {
   try {
-    await Blog.create({
+    const dbBlogData = await Blog.create({
+      ...req.body,
       title: req.body.title,
       content: req.body.content,
       user_id: req.session.user_id,
-    })
-    .then(dbBlogData => res.json(dbBlogData))
+    });
     res.status(200).json(dbBlogData);
   } catch (err) {
     res.status(400).json(err);
@@ -20,7 +20,7 @@ router.post("/", withAuth, async (req, res) => {
 // UPDATE a blog
 router.put("/:id", withAuth, async (req, res) => {
   try {
-    await Blog.update(
+    const dbBlogData = await Blog.update(
       {
         title: req.body.title,
         content: req.body.content,
@@ -30,13 +30,13 @@ router.put("/:id", withAuth, async (req, res) => {
           id: req.params.id,
         },
       }
-    ).then((dbBlogData) => {
+    )
       if (!dbBlogData) {
         res.status(404).json({ message: "No blog found with this ID" });
         return;
       }
       res.json(dbBlogData);
-    });
+
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -46,16 +46,18 @@ router.put("/:id", withAuth, async (req, res) => {
 // DELETE a blog
 router.delete("/:id", withAuth, async (req, res) => {
   try {
-    await Blog.destroy({
+    const dbBlogData = await Blog.destroy({
       where: {
         id: req.params.id,
+        user_id: req.session.user_id,
       },
-    }).then((dbBlogData) => {
-      if (!dbBlogData) {
-        res.status(404).json({ message: "No blog found with this id!" });
-        return;
-      }
     });
+
+    if (!dbBlogData) {
+      res.status(404).json({ message: 'No blog found with this ID!'});
+      return;
+    }
+
     res.status(200).json(dbBlogData);
   } catch (err) {
     res.status(500).json(err);
